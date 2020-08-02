243a1451073028389514234cacc7ccdf7f07ff19
silvaalcantara <allan.pa.br@gmail.com>
2020-08-02 16:57:21 -0300

    first commit

---
+++ boardgame/Board.java
@@ -0,0 +1,37 @@
+package boardgame;
+
+public class Board {
+
+    private int rows;
+    private int colums;
+    private Piece[][] pieces;
+
+    public Board(int rows, int colums) {
+        this.rows = rows;
+        this.colums = colums;
+    }
+
+    public int getRows() {
+        return this.rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColums() {
+        return this.colums;
+    }
+
+    public void setColums(int colums) {
+        this.colums = colums;
+    }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
+    }
+}

cc2246b06a10dbe9df648968920cbe0526a5eeea
silvaalcantara <allan.pa.br@gmail.com>
2021-01-14 15:51:14 -0300

    Imprimindo movimentos possiveis

--- chess/ChessMatch.java
+++ chess/ChessMatch.java
@@ -26,6 +26,12 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean [][] possibleMoves(ChessPosition sourPosition) {
+        Position position = sourPosition.toPosition();
+        validadateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targePosition) {
         Position source = sourcePosition.toPosition();
         Position target = targePosition.toPosition();

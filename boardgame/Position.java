1d44b2c5e6be6c866c7f7a8553121701571628f9
silvaalcantara <allan.pa.br@gmail.com>
2021-09-05 22:07:13 -0300

    fixes

--- boardgame/Position.java
+++ boardgame/Position.java
@@ -2,37 +2,37 @@ package boardgame;
 
 public class Position {
 
-    private int row;
-    private int column;
+	private int row;
+	private int column;
+	
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
-    public Position(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
+	public int getRow() {
+		return row;
+	}
 
-    public int getRow() {
-        return this.row;
-    }
+	public void setRow(int row) {
+		this.row = row;
+	}
 
-    public void setRow(int row) {
-        this.row = row;
-    }
+	public int getColumn() {
+		return column;
+	}
 
-    public int getColumn() {
-        return this.column;
-    }
-
-    public void setColumn(int column) {
-        this.column = column;
-    }
-
-    public void setValues(int row, int column) {
-        this.row = row;
-        this.column = column;
-    }
-
-    @Override
-    public String toString() {
-        return row + ", " + column;
-    }   
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
 }
--- chess/ChessPiece.java
+++ chess/ChessPiece.java
@@ -6,36 +6,36 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
-    private Color color;
-    private int moveCount;
+	private Color color;
+	private int moveCount;
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-    public Color getColor() {
-        return this.color;
-    }
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
 
-    public int getMoveCount() {
-        return moveCount;
-    }
- 
-    public void increaseMoveCount() {
-        moveCount++;
-    }
+	protected void decreaseMoveCount() {
+		moveCount--;
+	}
 
-    public void decreaseMoveCount() {
-        moveCount--;
-    }
-
-    public ChessPosition getChessPosition() {
-        return ChessPosition.fromPosition(position);
-    }
-
-    protected boolean isThereOpponentPiece(Position position) {
-        ChessPiece p = (ChessPiece) getBoard().piece(position);
-        return p != null && p.getColor() != color;
-    }
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
